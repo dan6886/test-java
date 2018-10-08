96918e4a0b2f8449503458c45513a0ec8ecf78db
cx.dan <cx.dan@ubtrobot.com>
2018-10-08 18:46:21 +0800

    fix:更新代码规范

--- src/main/java/deadlock/Main.java
+++ src/main/java/deadlock/Main.java
@@ -7,17 +7,17 @@ public class Main {
     public static void main(String[] arg) {
         Object obj1 = new Object();
         Object obj2 = new Object();
-        LockRunable runable1 = new LockRunable(obj1, obj2);
-        LockRunable runable2 = new LockRunable(obj2, obj1);
+        LockRunnable runable1 = new LockRunnable(obj1, obj2);
+        LockRunnable runable2 = new LockRunnable(obj2, obj1);
         new Thread(runable1, "r1").start();
         new Thread(runable2, "r2").start();
     }
 
-    private static class LockRunable implements Runnable {
+    private static class LockRunnable implements Runnable {
         private Object obj1;
         private Object obj2;
 
-        public LockRunable(Object obj1, Object obj2) {
+        public LockRunnable(Object obj1, Object obj2) {
             this.obj1 = obj1;
             this.obj2 = obj2;
         }
